d6c11d53741542bed33cfccf503c20801d5c307f
Joyal Johnson <159927024+jj662@users.noreply.github.com>
2026-03-03 20:48:26 +0530

    Remove gtag initialization from script.js
    Removed Google Analytics gtag initialization code.

--- js/script.js
+++ js/script.js
@@ -12,12 +12,6 @@
         });
         }
 
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX');
-       
-
         // --- Navbar Spotlight Logic ---
         function moveNavSpotlight(e) {
             const dock = e.currentTarget;
